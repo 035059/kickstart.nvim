678e2e6e3f4f78ee541086fe1bdadec3c42f54b6
035059 <allin@solarium.>
2024-03-07 00:22:25 -0500

    change autopairs to ultimate-autopairs

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -1,3 +1,8 @@
 return {
-  "jiangmiao/auto-pairs",
+  'altermo/ultimate-autopair.nvim',
+  event = { 'InsertEnter', 'CmdlineEnter' },
+  branch = 'v0.6', --recommended as each new version will have breaking changes
+  opts = {
+    --Config goes here
+  },
 }
