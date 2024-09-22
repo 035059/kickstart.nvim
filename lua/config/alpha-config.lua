a450372b75977f9df1b567661dc72d5d9193d483
Allin Demopolis <allindemopolis@gmail.com>
2024-09-22 13:32:18 -0400

    Revert to static homepage icon

--- lua/config/alpha-config.lua
+++ lua/config/alpha-config.lua
@@ -3,29 +3,29 @@ local dashboard = require("alpha.themes.dashboard")
 local ascii = require("ascii")
 
 -- Set header
---[[ Default header
+-- Default header
 dashboard.section.header.val = {
-  "                                                     ",
-  "  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
-  "  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
-  "  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
-  "  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
-  "  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
-  "  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
-  "                                                     ",
-} ]]
-dashboard.section.header.val = ascii.get_random_global()
+	"                                                     ",
+	"  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
+	"  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
+	"  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
+	"  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
+	"  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
+	"  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
+	"                                                     ",
+}
+--dashboard.section.header.val = ascii.get_random_global()
 
 -- Set menu
 dashboard.section.buttons.val = {
-  dashboard.button("e", "  > New file", ":ene <BAR> startinsert <CR>"),
-  dashboard.button("f", "󰈞  > Find file", ":cd $HOME/ | Telescope find_files<CR>"),
-  dashboard.button("r", "  > Recent", ":Telescope oldfiles<CR>"),
-  dashboard.button("p", "  > Projects", ":Telescope projects<CR>"),
-  dashboard.button("o", "  > File Explorer", ":Oil <CR>"),
-  dashboard.button('n', "󱞁 > Notes", ":Neorg workspace personal<CR>"),
-  dashboard.button("c", "  > Config", ":e $MYVIMRC <CR>"), --| :cd %:p:h | split . | wincmd k | pwd<CR>"),
-  dashboard.button("q", "󰗼  > Quit NVIM", ":qa<CR>"),
+	dashboard.button("e", "  > New file", ":ene <BAR> startinsert <CR>"),
+	dashboard.button("f", "󰈞  > Find file", ":cd $HOME/ | Telescope find_files<CR>"),
+	dashboard.button("r", "  > Recent", ":Telescope oldfiles<CR>"),
+	dashboard.button("p", "  > Projects", ":Telescope projects<CR>"),
+	dashboard.button("o", "  > File Explorer", ":Oil <CR>"),
+	dashboard.button("n", "󱞁 > Notes", ":Neorg workspace personal<CR>"),
+	dashboard.button("c", "  > Config", ":e $MYVIMRC <CR>"), --| :cd %:p:h | split . | wincmd k | pwd<CR>"),
+	dashboard.button("q", "󰗼  > Quit NVIM", ":qa<CR>"),
 }
 
 -- Send config to alpha
