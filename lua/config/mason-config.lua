eace736ead99c2ef376347a8137e495e75a61eb0
Allin Demopolis <allindemopolis@gmail.com>
2025-05-08 15:21:32 -0400

    laptop updates to config

--- lua/config/mason-config.lua
+++ lua/config/mason-config.lua
@@ -1,6 +1,4 @@
 -- [[ Mason Configuration ]]
--- mason-lspconfig requires that these setup functions are called in this order
--- before setting up the servers.
 require("mason").setup()
 require("mason-lspconfig").setup()
 local servers = {
@@ -10,31 +8,26 @@ local servers = {
 	rust_analyzer = {},
 	tsserver = {},
 	html = { filetypes = { "html", "twig", "hbs" } },
-
 	lua_ls = {
 		Lua = {
 			workspace = { checkThirdParty = false },
 			telemetry = { enable = false },
-			-- NOTE: toggle below to ignore Lua_LS's noisy `missing-fields` warnings
 			diagnostics = { disable = { "missing-fields" } },
 		},
 	},
 }
-
 -- Setup neovim lua configuration
 require("neodev").setup()
 
--- nvim-cmp supports additional completion capabilities, so broadcast that to servers
+-- CHANGED: Use blink.cmp instead of nvim-cmp
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
+capabilities = require("blink.cmp").get_lsp_capabilities(capabilities)
 
 -- Ensure the servers above are installed
 local mason_lspconfig = require("mason-lspconfig")
-
 mason_lspconfig.setup({
 	ensure_installed = vim.tbl_keys(servers),
 })
-
 mason_lspconfig.setup_handlers({
 	function(server_name)
 		require("lspconfig")[server_name].setup({
